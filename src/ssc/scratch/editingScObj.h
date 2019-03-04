779600ab5c952d8d7ca689a6529003051cb39288
Steve-xmh <Steve_xmh@163.com>
2019-03-04 19:07:07 +0800

    added obj struct

--- src/ssc/scratch/editingScObj.h
+++ src/ssc/scratch/editingScObj.h
@@ -4,9 +4,9 @@
 #include "../../ssc_libs.h"
 #include "ScratchSprite.h"
 
-//sscScratchSprite* ssc_editing_obj_get(); /**< 获取用户正在编辑的角色结构, NULL 则为选中了舞台 */
+sscScratchSprite* ssc_editing_obj_get(); /**< 获取用户正在编辑的角色结构, NULL 则为选中了舞台 */
 
-//void ssc_editing_obj_get() /**< 设置用户正在编辑的角色结构，如果为 NULL 则为舞台 */
+void ssc_editing_obj_get() /**< 设置用户正在编辑的角色结构，如果为 NULL 则为舞台 */
 
 
 
