417939ded09ba07d278d36508229dbfef2411430
Steve-xmh <Steve_xmh@163.com>
2019-03-04 13:23:40 +0800

    1

---
+++ src/ssc/scratch/editingScObj.c
@@ -0,0 +1,3 @@
+#include "editingScObj.h"
+
+
--- src/version.h
+++ src/version.h
@@ -2,29 +2,29 @@
 #define VERSION_H
 
 	//Date Version Types
-	#define SSC_VER_DATE "02"
+	#define SSC_VER_DATE "04"
 	#define SSC_VER_MONTH "03"
 	#define SSC_VER_YEAR "2019"
 	#define SSC_VER_UBUNTU_VERSION_STYLE  "19.03"
 	
 	//Software Status
-	#define SSC_VER_STATUS  "Alpha"
-	#define SSC_VER_STATUS_SHORT  "a"
+	#define SSC_VER_STATUS  ""
+	#define SSC_VER_STATUS_SHORT  ""
 	
 	//Standard Version Type
-	#define SSC_VER_MAJOR  1
+	#define SSC_VER_MAJOR  0
 	#define SSC_VER_MINOR  0
-	#define SSC_VER_BUILD  7
-	#define SSC_VER_REVISION  36
+	#define SSC_VER_BUILD  1
+	#define SSC_VER_REVISION  6
 	
 	//Miscellaneous Version Types
-	#define SSC_VER_BUILDS_COUNT  18
-	#define SSC_VER_RC_FILEVERSION 1,0,7,36
-	#define SSC_VER_RC_FILEVERSION_STRING "1, 0, 7, 36\0"
-	#define SSC_VER_FULLVERSION_STRING  "1.0.7.36"
+	#define SSC_VER_BUILDS_COUNT  2
+	#define SSC_VER_RC_FILEVERSION 0,0,1,6
+	#define SSC_VER_RC_FILEVERSION_STRING "0, 0, 1, 6\0"
+	#define SSC_VER_FULLVERSION_STRING  "0.0.1.6"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define SSC_VER_BUILD_HISTORY  7
+	#define SSC_VER_BUILD_HISTORY  1
 	
 
 #endif //VERSION_H
