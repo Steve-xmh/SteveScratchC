d8cf0a3f72145ec192a9dd7587e3e97a2198f078
Eureka0225 <1441373096@qq.com>
2019-03-04 23:24:32 +0800

    Delete Src

--- src/ssc/block/shapeDraw/cmd.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEADER_SSC_BLOCK_SHAPEDRAW_CMD
-#define HEADER_SSC_BLOCK_SHAPEDRAW_CMD
-
-#include "../../../ssc_libs.h"
-#include "../blockShape.h"
-
-void ssc_block_shape_draw_cmd(cairo_t*,guint,guint);
-
-#endif // HEADER_SSC_BLOCK_SHAPEDRAW_CMD
--- src/ssc/widget/widgetEvents.h
+++
@@ -1,8 +0,0 @@
-#ifndef HEADER_SSC_WIDGET_WIDGETEVENTS
-#define HEADER_SSC_WIDGET_WIDGETEVENTS
-
-#include <gtk/gtk.h>
-
-void ssc_widget_event_window_destroy(GtkWidget *widget,gpointer data);
-
-#endif // HEADER_SSC_WIDGET_WIDGETEVENTS
